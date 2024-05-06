b2186e4eb1430967c7b9fb19899018da0d29d76d
Albert Kapitanov <1442286+kapitanov@users.noreply.github.com>
2024-05-07 00:02:02 +0300

    Fix origin URL

--- src/core/shortener.js
+++ src/core/shortener.js
@@ -1,20 +1,31 @@
 import { compress, decompress } from "brotli-compress";
 import { Chars, BufferEncoder } from "bufferbase";
 
-const customDictionary = Buffer.from('https.com/?#')
+const customDictionary = Buffer.from("https.com/?#");
 
 const encoder = new BufferEncoder(Chars.Base64_URL_SAFE);
 const utf16Decoder = new TextDecoder("UTF-8");
+
+function rootUrl() {
+  let url = window.location.href;
+
+  while (url.endsWith("/")) {
+    url = url.substring(0, url.length - 1);
+  }
+  return url;
+}
 
 export async function shorten(url) {
   const inputBuffer = Buffer.from(url);
   const compressedBuffer = await compress(inputBuffer, { customDictionary });
   const compressesString = encoder.encode(compressedBuffer);
-  const resultUrl = `${window.location.origin}?${compressesString}`;
+  const resultUrl = `${rootUrl()}?${compressesString}`;
 
   const compressionRatio = (100 * compressedBuffer.length) / inputBuffer.length;
   console.log(`Shortened "${url}" to "${resultUrl}":`);
-  console.log(`  ${inputBuffer.length} bytes -> ${compressedBuffer.length} bytes`);
+  console.log(
+    `  ${inputBuffer.length} bytes -> ${compressedBuffer.length} bytes`
+  );
   console.log(`  Compression ratio: ${compressionRatio.toFixed(2)}%`);
 
   return resultUrl;
@@ -23,7 +34,9 @@ export async function shorten(url) {
 export async function unshorten(url) {
   url = url.substr(1);
   const compressedBuffer = encoder.decode(url);
-  const decompressedBuffer = await decompress(compressedBuffer, { customDictionary });
+  const decompressedBuffer = await decompress(compressedBuffer, {
+    customDictionary,
+  });
   const decompressedString = utf16Decoder.decode(decompressedBuffer);
   return decompressedString;
 }
